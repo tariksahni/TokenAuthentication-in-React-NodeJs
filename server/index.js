1e7f54ad3c014a758dfc0b47f9f9c334424ef543
Tarik Sahni <tariksahni@gmail.com>
2018-01-22 13:52:44 +0530

    More Server SetUp

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//App SetUp
+
+//Server SetUp
+
+const port = process.env.PORT || 3333;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server is listening on", port);
