b4693353fc8c7f7236df1659264c32b6e327dff9
Tarik Sahni <tariksahni@gmail.com>
2018-01-29 12:14:09 +0530

    Passport JwtStrategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,3 +3,19 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// set up options for JWT Strategy
+const jwtOptions = {};
+
+// create the strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) return done(err, false);
+    if (user) {
+      return done(null, user);
+    } else return done(null, false);
+  });
+});
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
